d902fe38143973e5f73988aa551648d4ec2f8295
Borjaarmet <borjaarmet@gmail.com>
2021-07-19 14:52:50 +0200

    create Create recipe page component

---
+++ src/pages/CreateRecipe.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreateRecipe extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Create a recipe</h1>
+      </div>
+    )
+  }
+}
