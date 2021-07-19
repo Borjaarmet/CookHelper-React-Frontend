cc475266544586a7238bd8b3b554f77ff6601109
Borjaarmet <borjaarmet@gmail.com>
2021-07-19 14:53:22 +0200

    create TitleSection folder

---
+++ src/components/TitleSection/index.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import Video from "../../video/pexels-miguel-á-padriñán-6122138.mp4";
+
+// import { Button } from "../ButtonElement";
+
+import {
+  TitleContainer,
+  TitleBg,
+  TitleVideoBg,
+  TitleContent,
+  TitleText,
+  TitleH1,
+  TitleP,
+  TitleIcons,
+  IconLink,
+  TitleButton,
+  Button,
+  // ArrowForward,
+  // ArrowRight,
+} from "./titleElements";
+import {
+  TiSocialLinkedin,
+  TiSocialFacebook,
+  TiSocialGithub,
+  TiSocialInstagram,
+} from "react-icons/ti";
+
+
+class TitleSection extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      hover:false
+    }
+  }
+
+  render(){
+  return (
+    <TitleContainer id="home">
+      <TitleBg>
+        <TitleVideoBg autoPlay loop muted src={Video} type="video/mp4" />
+      </TitleBg>
+      <TitleContent>
+        <TitleText>
+          <TitleH1>COOK HELPER</TitleH1>
+          <TitleP>
+            We are an App that help you to find a recipe from the ingredients
+            that you have in your fridge.Enjoy cooking!!
+          </TitleP>
+
+          <TitleIcons>
+            <IconLink
+              to={{
+                pathname: "https://www.linkedin.com/in/borja-armet-alonso/",
+              }}
+              target="_blank"
+            >
+              <TiSocialLinkedin />
+            </IconLink>
+            <IconLink
+              to={{
+                pathname: "https://instagram.com/Borjaarmet",
+              }}
+              target="_blank"
+            >
+              <TiSocialInstagram />
+            </IconLink>
+            <IconLink
+              to={{
+                pathname: "https://github.com/Borjaarmet",
+              }}
+              target="_blank"
+            >
+              <TiSocialGithub />
+            </IconLink>
+            <IconLink
+              to={{
+                pathname: "https://facebook.com/Borjaarmet",
+              }}
+              target="_blank"
+            >
+              <TiSocialFacebook />
+            </IconLink>
+          </TitleIcons>
+        </TitleText>
+        <TitleButton>
+          <Button>
+            Get started
+          </Button>
+          {/* <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button> */}
+        </TitleButton>
+      </TitleContent>
+    </TitleContainer>
+  );
+ }
+};
+
+export default TitleSection;
---
+++ src/components/TitleSection/titleElements.js
@@ -0,0 +1,150 @@
+import styled from "styled-components";
+import { MdKeyboardArrowRight, MdArrowForward } from "react-icons/md";
+
+export const TitleContainer = styled.div`
+  background: #0c0c0c;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0 30px;
+  height: 500px;
+  position: relative;
+  z-index: 1;
+
+  :before {
+    content: "";
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: linear-gradient(
+        180deg,
+        rgba(0, 0, 0, 0.2) 100%,
+        rgba(0, 0, 0, 0.8) 100%
+      ),
+      linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, transparent 100%);
+    z-index: 2;
+  }
+`;
+
+export const TitleBg = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
+
+export const TitleVideoBg = styled.video`
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+  background: #232a34;
+`;
+
+export const TitleContent = styled.div`
+  z-index: 3;
+  max-width: 1200px;
+  position: absolute;
+  padding: 8px 24px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TitleText = styled.div`
+  z-index: 3;
+  max-width: 1200px;
+
+  padding: 8px 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TitleH1 = styled.h1`
+  color: #fff;
+  font-size: 40px;
+  text-align: center;
+
+  @media screen and (max-width: 768px) {
+    font-size: 32px;
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 24px;
+  }
+`;
+
+export const TitleP = styled.p`
+  margin-top: 24px;
+  color: #fff;
+  font-size: 24px;
+  text-align: center;
+  max-width: 600px;
+
+  @media screen and (max-width: 768px) {
+    font-size: 20px;
+  }
+
+  @media screen and (max-width: 480px) {
+    font-size: 12px;
+  }
+`;
+
+export const TitleIcons = styled.div`
+  margin-top: 30px;
+  display: flex;
+  font-size: 40px;
+  text-align: center;
+  max-width: 600px;
+`;
+
+export const TitleButton = styled.div`
+  margin-top: 32px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ArrowForward = styled(MdArrowForward)`
+  margin-left: 8px;
+  font-size: 20px;
+`;
+
+export const ArrowRight = styled(MdKeyboardArrowRight)`
+  margin-left: 8px;
+  font-size: 20px;
+`;
+
+export const IconLink = styled.p`
+  font-size: 26px;
+  color: #fff;
+  cursor: pointer;
+  margin-right: 15px;
+  border: 1px solid white;
+  border-radius: 25px;
+  padding: 3px 5px;
+`;
+
+export const Button = styled.button`
+ background-color: #01bf71;
+  border-radius: 50px;
+  padding: 5px 10px;
+  border: none;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 16px;
+  outline: none;
+  white-space: nowrap;
+  color: #010606;
+  transition: all 0.2s ease-in-out;
+  margin-right: 10px;
+`
+
