0355a7bdd455e152902b067010e423a5f134e1fd
Borjaarmet <borjaarmet@gmail.com>
2021-07-20 20:32:13 +0200

    add onchange function and change name on a classname attribute

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -28,6 +28,12 @@ import { withAuth } from '../providers/AuthProvider';
    } 
   };
 
+  handleChange = (event) => {
+    this.setState({
+
+    })
+  }
+
   render() {
     const {user, status} = this.state;
     return (
@@ -36,56 +42,55 @@ import { withAuth } from '../providers/AuthProvider';
         <div>
          {status === 'loading' && <div>loading</div>}
          {status === 'loaded' && 
-    <div className='profile-form'>
-     <form >
-      <label className='label-profile' htmlFor='name'>Username</label>
-      <br />
-      <input className='input-profile' type='text' name='username' id='name' value={user.username} />
-      <br />
-      <label className='label-profile' htmlFor='email'>Email</label>
-      <br />
-      <input className='input-profile' type='text' name='email' id='email' value={user.email} />
-      <br />
-      <label className='label-profile' htmlFor='country'>Nationality</label>
-      <br />
-      <input
-        className='input-profile'
-        type='text'
-        name='nationality'
-        id='nationality'
-        value={user.nationality}
-      />
-      <br />
-      <label className='label-profile' htmlFor='age'>Age</label>
-      <br />
-      <input
-        className='input-profile'
-        type='number'
-        name='age'
-        id='age'
-        placeholder='Your age'
-        value={user.age}
-      />
-
-      <label className='label-profile' htmlFor='cookLevel'>Cook level <br />{user.cookLevel}</label>
-      <br />
-
-      <select name='cookLevel' id='cookLevel'>
-        <option value='none' selected></option>
-        <option value='I have no idea'>I have no idea</option>
-        <option value='I defend myself'>I defend myself</option>
-        <option value='Advanced'>Advanced</option>
-        <option value='I´m a chef'>I´m a chef</option>
-      </select>
-
-      <label className='label-profile' htmlFor='favourites'>Favourite recipes</label>
-      <br />
-      <button className='submit' type='submit'>Save Changes</button>
-
-    </form>
-
-  </div>
-         }
+         <form >
+          <div className='container-form'>
+            
+              <label className='label-profile' htmlFor='name'>Username</label>
+              <br />
+              <input className='input-profile' type='text' name='username' id='name' value={user.username} onChange={this.handleChange} />
+              <br />
+              <label className='label-profile' htmlFor='email'>Email</label>
+              <br />
+              <input className='input-profile' type='text' name='email' id='email' value={user.email} onChange={this.handleChange} />
+              <br />
+              <label className='label-profile' htmlFor='country' >Nationality</label>
+              <br />
+              <input
+                className='input-profile'
+                type='text'
+                name='nationality'
+                id='nationality'
+                value={user.nationality}
+                onChange={this.handleChange}
+              />
+              <br />
+              <label className='label-profile' htmlFor='age'>Age</label>
+              <br />
+              <input
+                className='input-profile'
+                type='number'
+                name='age'
+                id='age'
+                placeholder='Your age'
+                value={user.age}
+                onChange={this.handleChange}
+              />
+              <br/>
+              <label className='label-profile' htmlFor='cookLevel'>Cook level <br />{user.cookLevel}</label>
+              <br />
+              <select name='cookLevel' id='cookLevel'>
+                <option value='none' selected></option>
+                <option value='I have no idea'>I have no idea</option>
+                <option value='I defend myself'>I defend myself</option>
+                <option value='Advanced'>Advanced</option>
+                <option value='I´m a chef'>I´m a chef</option>
+              </select>
+              <br />
+              <button className='submit' type='submit'>Save Changes</button>
+            
+          </div>
+          </form>
+        }
         </div>
       </>
     )
