0afc2490a80f9a631151d597ec46211cd6121bda
Borjaarmet <borjaarmet@gmail.com>
2021-07-22 21:25:49 +0200

    import navbar and add onClick function

--- src/pages/CreateList.js
+++ src/pages/CreateList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import userClient from '../lib/userClient'
+import userClient from '../lib/userClient';
+import Navbar from '../components/Navbar'
 
 class CreateList extends Component {
   constructor(props){
@@ -24,20 +25,43 @@ class CreateList extends Component {
     console.log(error)
    } 
   };
+
+  deleteRecipe = async () => {
+    try{
+       const deletedRecipe = await userClient.getUserDeleteRecipeCreated(this.props.match.params.id) 
+      console.log("deleted recipe:" , deletedRecipe)
+      this.setState({
+        createdList: this.state.createdList.slice(1, deletedRecipe)
+      })
+    }
+    catch{
+      console.log("eerror")
+    }
+  };
+
+  // editRecipe = async() => {
+  //   try{
+  //     const updateRecipe = await userClient
+  //   }
+  // }
     
   render() {
     const {createdList} = this.state
-    console.log("render createdList", this.state.createdList)
+    console.log("render createdList", this.state)
     return (
-      <div>
+      <>
+        <Navbar/>
         <h1>My own recipes</h1>
         <p>You have {createdList.length} recipes!</p>
           <ul>
            {createdList.map((recipe, index) => {
-            return <li key={index}>{recipe.ingredientsList}</li>
+            return <li key={index}>{recipe._id}
+            <button onClick = {this.deleteRecipe}>delete</button>
+            <button onClick = {this.editRecipe}>edit</button>
+            </li>
           })}  
         </ul>  
-      </div>
+      </>
     )
   };
 };
