9dfc763e954af83cf1ef2b1eab4716b6c80db0c3
Borjaarmet <borjaarmet@gmail.com>
2021-07-22 21:22:53 +0200

    change some structure for CSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withAuth } from '../providers/AuthProvider';
+import { FaBars } from "react-icons/fa";
 import '../App.css'
 
 class Navbar extends Component {
@@ -9,53 +10,58 @@ class Navbar extends Component {
 		return (
 			<>
 				{isLoggedIn ? (
-					
+					<div className="nav">
 					<div className="navContainer">
 						<div className="navLogo">
-						 <p> {user.username}</p>
+						 <Link to="/app">
+							<h3 >CookHelper</h3>
+						</Link>
 						</div>
-						<div className="navLinks">
-							<div >
+						<div className="navMenu">
+							<div className="navItem">
 						   <Link to={`/user/${user._id}/profile`}>
 						    <p className="navLink">Profile</p>
 						   </Link>
 						  </div>
-							<div>
+							<div className="navItem">
 						   <Link to={`/user/favourites`}>
 						    <p className="navLink">FavList</p>
 						   </Link>
 						  </div>
-							<div>
+							<div className="navItem">
 						   <Link to={`/user/create`}>
 						    <p className="navLink">CreatedList</p>
 						   </Link>
 						  </div>
-							<div>
+							<div className="navItem">
 						   <Link to={`/user/createRecipe`}>
 						    <p className="navLink">Create recipe</p>
 						   </Link>
 						  </div>
 						</div>
-						<div className="navBtns">
-						<button className="navBtn"  onClick={logout}>Logout</button>
+						<div className="navBtn">
+						<button className="navBtnLink"  onClick={logout}>Logout</button>
+						</div>
+						<div className="mobileIcon">
+							<FaBars/>
 						</div>
 					</div>
-					
+					</div>
 				) : (
-					
+					<div className="nav">
 					<div className="navContainer">
 						<div className="navLogo">
 					  <Link to="/">
 							<h3 >CookHelper</h3>
 						</Link>
 						</div>
-						<div className="navLinks">
-							<div >
+						<div className="navMenu">
+							<div className="navItem">
 								<Link to="/login">
 									<i className="navLink">About us</i>
 								</Link>
 							</div>
-							<div>
+							<div className="navItem">
 								<Link  to="/login">
 									<i className="navLink">Some recipes</i>
 								</Link>
@@ -72,10 +78,12 @@ class Navbar extends Component {
 					   			<button className="navBtn">Signup</button>
 								</Link>
 						  </div>
+							<div className="mobileIcon">
+							<FaBars/>
 						</div>
-						
+						</div>	
 					</div>
-					
+					</div>
 				)}
 			</>
 		);
