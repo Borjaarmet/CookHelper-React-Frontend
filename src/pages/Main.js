8960002f3bd82019b9b127c0a52730c49490c546
Borjaarmet <borjaarmet@gmail.com>
2021-07-23 21:02:46 +0200

    add handleSearchRecipe to see a recipe from search input

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -13,22 +13,17 @@ import recipeClient from '../lib/recipeClient';
 import {Link} from 'react-router-dom';
 
 
+
 class Main extends Component {
   constructor(props){
     super(props)
     this.state={
       initialRecipes: [],
-      ShownRecipes: [],
+      shownRecipes: [],
       searchValue: "",
+      
     }
   }
-
-  
-  handleSearchValue = (searchValue) => {
-    this.setState({
-      searchValue: searchValue,
-    });
-  };
 
   componentDidMount = () => {
     console.log("compdidmount")
@@ -45,16 +40,63 @@ class Main extends Component {
     };
   };
 
+  handleSearchValue = (event) => {
+    console.log(event.target.name)
+    console.log(event.target.value)
+    this.setState({
+      searchValue: event.target.value,
+    });
+  };
+
+  handleSearchRecipe = () => {
+   const {searchValue} = this.state;
+   console.log("searchvalue :", searchValue)
+   const newCopy = [...this.state.initialRecipes]
+   console.log("newCopy: ", newCopy)
+   const recipesFounded = newCopy.filter((item) => {
+        return item.recipeName === searchValue
+     })
+   console.log("recipes founded: ", recipesFounded)
+   this.setState({
+     shownRecipes: this.state.shownRecipes.push(recipesFounded)
+   })
+  };
+  
 	render() {
     console.log(this.state.initialRecipes)
+    const {shownRecipes} = this.state
+     console.log("shownRecipes:" ,shownRecipes)
 		return (
 			<>
         <Navbar/>
-				<SearchInput onSearchValue = {this.handleSearchValue} value= {this.state.searchValue}/>
-				<div>
+				<SearchInput 
+          value={this.state.searchValue}
+          onSearchValue={this.handleSearchValue}
+          onSearchRecipe= {this.handleSearchRecipe}
+          />
+
+				<div className="recipes-container">
           <h1>Some recipes</h1>
            <div className="recipe-box">
-          { this.state.initialRecipes.map( (recipe) => {
+            
+             {shownRecipes.map((recipe) => {
+               return <div key={recipe._id}>
+                 <Link to={`/recipes/${recipe._id}/details`}>
+                  <h4>{recipe.recipeName}</h4>
+                </Link>
+                 <iframe
+                    width='200'
+                    height='200'
+                    src={recipe.videoLink}
+                    title='YouTube video player'
+                    frameBorder='0'
+                    allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                    allowFullScreen
+                 ></iframe>
+               </div>
+             })}
+          {/* { this.state.initialRecipes.map( (recipe) => {
+           
             return (
               <div key={recipe._id}>
                 
@@ -72,10 +114,11 @@ class Main extends Component {
                  ></iframe>
               </div>
             )})
-          }
+          } */}
         </div>  
 				</div>
-         
+        
+          
 			</>
 		);
 	}
