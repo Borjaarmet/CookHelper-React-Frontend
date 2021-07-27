6002a6444f4fec267b6b5844f1821ccf11e88d48
Borjaarmet <borjaarmet@gmail.com>
2021-07-27 14:46:43 +0200

    create DairyBoxCheck component

--- src/components/DairyBoxCheck.js
+++ src/components/DairyBoxCheck.js
@@ -26,22 +26,22 @@ class DairyBoxCheck extends Component {
   render() {
     return (
       <>
-        <input className='input-searchBox' type='checkbox' name='butter' value={this.state.butter} onChange={this.handleChange}/>
-              <label htmlFor='dairy1'> Butter</label><br />
-              <input className='input-searchBox' type='checkbox' name='milk' value={this.state.milk} onChange={this.handleChange}/>
-              <label htmlFor='dairy2'> Milk</label><br />
-              <input className='input-searchBox' type='checkbox' name='eggs' value={this.state.eggs} onChange={this.handleChange}/>
-              <label htmlFor='dairy3'> Eggs</label><br />
-              <input className='input-searchBox' type='checkbox' name='sour cream' value={this.state.sourCream} onChange={this.handleChange} />
-              <label htmlFor='dairy4'> Sour cream</label><br />
-              <input className='input-searchBox' type='checkbox' name='coco milk' value={this.state.cocoMilk} onChange={this.handleChange}/>
-              <label htmlFor='dairy5'> Coco milk</label><br />
-              <input className='input-searchBox' type='checkbox' name='emmentaler cheese' value={this.state.emmentalerCheese} onChange={this.handleChange}/>
-              <label htmlFor='dairy6'> Emmentaler cheese</label><br />
-              <input className='input-searchBox' type='checkbox' name='gouda cheese' value={this.state.goudaCheese} onChange={this.handleChange}/>
-              <label htmlFor='dairy7'> Gouda cheese</label><br />
-              <input className='input-searchBox' type='checkbox' name='cheddar cheese' value={this.state.cheddarCheese} onChange={this.handleChange}/>
-              <label htmlFor='dairy8'> Cheddar cheese</label><br />
+        <input className='input-searchBox' type='checkbox' name='butter' id="butter" value={this.state.butter} onChange={this.handleChange}/>
+              <label htmlFor='butter'> Butter</label><br />
+              <input className='input-searchBox' type='checkbox' name='milk' id="milk" value={this.state.milk} onChange={this.handleChange}/>
+              <label htmlFor='milk'> Milk</label><br />
+              <input className='input-searchBox' type='checkbox' name='eggs' id="eggs" value={this.state.eggs} onChange={this.handleChange}/>
+              <label htmlFor='eggs'> Eggs</label><br />
+              <input className='input-searchBox' type='checkbox' name='sour cream' id="sourCream" value={this.state.sourCream} onChange={this.handleChange} />
+              <label htmlFor='sourCream'> Sour cream</label><br />
+              <input className='input-searchBox' type='checkbox' name='coco milk' id="cocoMilk" value={this.state.cocoMilk} onChange={this.handleChange}/>
+              <label htmlFor='cocoMilk'> Coco milk</label><br />
+              <input className='input-searchBox' type='checkbox' name='emmentaler cheese' id="emmentalerCheese" value={this.state.emmentalerCheese} onChange={this.handleChange}/>
+              <label htmlFor='emmentalerCheese'> Emmentaler cheese</label><br />
+              <input className='input-searchBox' type='checkbox' name='gouda cheese' id="goudaCheese" value={this.state.goudaCheese} onChange={this.handleChange}/>
+              <label htmlFor='goudaCheese'> Gouda cheese</label><br />
+              <input className='input-searchBox' type='checkbox' name='cheddar cheese' id="cheddarCheese" value={this.state.cheddarCheese} onChange={this.handleChange}/>
+              <label htmlFor='cheddarCheese'> Cheddar cheese</label><br />
       </>
     )
   }
