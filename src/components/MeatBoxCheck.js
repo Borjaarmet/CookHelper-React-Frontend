61fa9d36b9787288d7cb658a771182c1282865b1
Borjaarmet <borjaarmet@gmail.com>
2021-07-27 14:48:22 +0200

    create MeatBoxCheck component

--- src/components/MeatBoxCheck.js
+++ src/components/MeatBoxCheck.js
@@ -17,25 +17,25 @@ class MeatBoxCheck extends Component {
     console.log(event.target.name)
     console.log(event.target.checked)
     this.setState({
-      [event.target.name]: event.target.checked
+      [event.target.name]: event.target.value
     })
   };
 
   render() {
     return (
       <>
-        <input className='input-searchBox' type='checkbox' name='pork' value={this.state.pork} onChange={this.handleChange} />
-              <label> Pork</label><br />
-              <input className='input-searchBox' type='checkbox' name='chicken' value={this.state.chicken} onChange={this.handleChange} />
-              <label> Chicken</label><br />
-              <input className='input-searchBox' type='checkbox' name='hamburguer' value={this.state.hamburguer} onChange={this.handleChange} />
-              <label> Hamburguer</label><br />
-              <input className='input-searchBox' type='checkbox' name='lamb' value={this.state.lamb} onChange={this.handleChange} />
-              <label> Lamb</label><br />
-              <input className='input-searchBox' type='checkbox' name='bacon' value={this.state.bacon} onChange={this.handleChange} />
-              <label> Bacon</label><br />
-              <input className='input-searchBox' type='checkbox' name='beef' value={this.state.beef} onChange={this.handleChange} />
-              <label> Beef</label><br />
+        <input className='input-searchBox' type='checkbox' name='pork' id="pork" value={this.state.pork} onChange={this.handleChange} />
+              <label htmlFor="pork"> Pork</label><br />
+              <input className='input-searchBox' type='checkbox' name='chicken' id="chicken" value={this.state.chicken} onChange={this.handleChange} />
+              <label htmlFor="chicken"> Chicken</label><br />
+              <input className='input-searchBox' type='checkbox' name='hamburguer' id="hamburguer" value={this.state.hamburguer} onChange={this.handleChange} />
+              <label htmlFor="hamburguer"> Hamburguer</label><br />
+              <input className='input-searchBox' type='checkbox' name='lamb' id="lamb" value={this.state.lamb} onChange={this.handleChange} />
+              <label htmlFor="lamb"> Lamb</label><br />
+              <input className='input-searchBox' type='checkbox' name='bacon' id="bacon" value={this.state.bacon} onChange={this.handleChange} />
+              <label htmlFor="bacon"> Bacon</label><br />
+              <input className='input-searchBox' type='checkbox' name='beef' id="beef" value={this.state.beef} onChange={this.handleChange} />
+              <label htmlFor="beef"> Beef</label><br />
       </>
     )
   }
