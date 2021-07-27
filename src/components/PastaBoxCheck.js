6e1ecc2f5b99c2f37e6921f9c9d20500dd5c55ee
Borjaarmet <borjaarmet@gmail.com>
2021-07-27 14:48:54 +0200

    create PastaBoxCheck component

--- src/components/PastaBoxCheck.js
+++ src/components/PastaBoxCheck.js
@@ -24,18 +24,18 @@ class PastaBoxCheck extends Component {
   render() {
     return (
       <>
-         <input className='input-searchBox' type='checkbox' name='macarroni' value={this.state.macarroni} onChange={this.handleChange}/>
-            <label> Macarroni</label><br />
-            <input className='input-searchBox' type='checkbox' name='spaghetti' value={this.state.spaghetti} onChange={this.handleChange}/>
-            <label> Spaghetti</label><br />
-            <input className='input-searchBox' type='checkbox' name='ravioli' value={this.state.raviolis} onChange={this.handleChange}/>
-            <label> raviolis</label><br />
-            <input className='input-searchBox' type='checkbox' name='noodles' value={this.state.noodles} onChange={this.handleChange}/>
-            <label> Nooddles</label><br />
-            <input className='input-searchBox' type='checkbox' name='rice' value={this.state.rice} onChange={this.handleChange}/>
-            <label> Rice</label><br />
-            <input className='input-searchBox' type='checkbox' name='quinoa' value={this.state.quinoa} onChange={this.handleChange} />
-            <label> Quinoa</label><br />
+         <input className='input-searchBox' type='checkbox' name='macarroni' id="macarroni" value={this.state.macarroni} onChange={this.handleChange}/>
+            <label htmlFor="macarroni"> Macarroni</label><br />
+            <input className='input-searchBox' type='checkbox' name='spaghetti' id="spaghetti"  value={this.state.spaghetti} onChange={this.handleChange}/>
+            <label htmlFor="spaghetti"> Spaghetti</label><br />
+            <input className='input-searchBox' type='checkbox' name='ravioli' id="raviolis" value={this.state.raviolis} onChange={this.handleChange}/>
+            <label htmlFor="raviolis"> raviolis</label><br />
+            <input className='input-searchBox' type='checkbox' name='noodles' id="noodles" value={this.state.noodles} onChange={this.handleChange}/>
+            <label htmlFor="noodles"> Nooddles</label><br />
+            <input className='input-searchBox' type='checkbox' name='rice' id="rice" value={this.state.rice} onChange={this.handleChange}/>
+            <label htmlFor="rice"> Rice</label><br />
+            <input className='input-searchBox' type='checkbox' name='quinoa' id="quinoa" value={this.state.quinoa} onChange={this.handleChange} />
+            <label htmlFor="quinoa"> Quinoa</label><br />
       </>
     )
   }
