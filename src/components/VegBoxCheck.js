1402eea470ef7717cc69d6566de5b1295d5eb760
Borjaarmet <borjaarmet@gmail.com>
2021-07-27 14:49:51 +0200

    create VegBoxCheck component

--- src/components/VegBoxCheck.js
+++ src/components/VegBoxCheck.js
@@ -28,26 +28,26 @@ class VegBoxCheck extends Component {
   render() {
     return (
       <>
-         <input className='input-searchBox' type='checkbox' name='tomatoes' value={this.state.tomatoes} onChange={this.handleChange}/>
-            <label>Tomatoes</label><br />
-            <input className='input-searchBox' type='checkbox' name='peppers' value={this.state.peppers} onChange={this.handleChange}/>
-            <label> Peppers</label><br />
-            <input className='input-searchBox' type='checkbox' name='onions' value={this.state.onions} onChange={this.handleChange}/>
-            <label> Onions</label><br />
-            <input className='input-searchBox' type='checkbox' name='cucumber' value={this.state.cucumber} onChange={this.handleChange}/>
-            <label> Cucumber</label><br />
-            <input className='input-searchBox' type='checkbox' name='carrots' value={this.state.carrots} onChange={this.handleChange}/>
-            <label> Carrots</label><br />
-            <input className='input-searchBox' type='checkbox' name='zuchinni' value={this.state.zuchini} onChange={this.handleChange}/>
-            <label> Zuchini</label><br />
-            <input className='input-searchBox' type='checkbox' name='pumpkin' value={this.state.pumpkin} onChange={this.handleChange}/>
-            <label> Pumpkin</label><br />
-            <input className='input-searchBox' type='checkbox' name='avocados' value={this.state.avocados} onChange={this.handleChange}/>
-            <label> Avocados</label><br />
-            <input className='input-searchBox' type='checkbox' name='eggplants' value={this.state.eggplants} onChange={this.handleChange}/>
-            <label> Eggplants</label><br />
-            <input className='input-searchBox' type='checkbox' name='mushrooms' value={this.state.mushrooms} onChange={this.handleChange}/>
-            <label> Mushrooms</label><br />
+         <input className='input-searchBox' type='checkbox' name='tomatoes' id="tomatoes" value={this.state.tomatoes} onChange={this.handleChange}/>
+            <label htmlFor="tomatoes">Tomatoes</label><br />
+            <input className='input-searchBox' type='checkbox' name='peppers' id="peppers" value={this.state.peppers} onChange={this.handleChange}/>
+            <label htmlFor="peppers"> Peppers</label><br />
+            <input className='input-searchBox' type='checkbox' name='onions' id="onions" value={this.state.onions} onChange={this.handleChange}/>
+            <label htmlFor="onions"> Onions</label><br />
+            <input className='input-searchBox' type='checkbox' name='cucumber' id="cucumber" value={this.state.cucumber} onChange={this.handleChange}/>
+            <label htmlFor="cucumber"> Cucumber</label><br />
+            <input className='input-searchBox' type='checkbox' name='carrots' id="carrots" value={this.state.carrots} onChange={this.handleChange}/>
+            <label htmlFor="carrots"> Carrots</label><br />
+            <input className='input-searchBox' type='checkbox' name='zuchinni' id="zuchini" value={this.state.zuchini} onChange={this.handleChange}/>
+            <label htmlFor="zuchini"> Zuchini</label><br />
+            <input className='input-searchBox' type='checkbox' name='pumpkin' id="pumpkin" value={this.state.pumpkin} onChange={this.handleChange}/>
+            <label htmlFor="pumpkin"> Pumpkin</label><br />
+            <input className='input-searchBox' type='checkbox' name='avocados' id="avocados" value={this.state.avocados} onChange={this.handleChange}/>
+            <label htmlFor="avocados"> Avocados</label><br />
+            <input className='input-searchBox' type='checkbox' name='eggplants' id="eggplants" value={this.state.eggplants} onChange={this.handleChange}/>
+            <label htmlFor="eggplants"> Eggplants</label><br />
+            <input className='input-searchBox' type='checkbox' name='mushrooms' id="mushrooms" value={this.state.mushrooms} onChange={this.handleChange}/>
+            <label htmlFor="mushrooms"> Mushrooms</label><br />
       </>
     )
   }
