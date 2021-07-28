ef06fadf44ef454778a4150a930a87e2644c9807
Borjaarmet <borjaarmet@gmail.com>
2021-07-28 08:31:08 +0200

    creat

--- src/pages/CreateList.js
+++ src/pages/CreateList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import userClient from '../lib/userClient';
-import Navbar from '../components/Navbar'
+import Navbar from '../components/Navbar';
+import { Link } from 'react-router-dom';
 
 class CreateList extends Component {
   constructor(props){
@@ -52,16 +53,47 @@ class CreateList extends Component {
     return (
       <>
         <Navbar/>
-        <h1>My own recipes</h1>
+        {/* <h1>My own recipes</h1>
         <p>You have {createdList.length} recipes!</p>
           <ul>
            {createdList.map((recipe, index) => {
-            return <li key={index}>{recipe._id}
+            return <li key={index}>{recipe._id}, {recipe.recipeName}
             <button onClick = {this.deleteRecipe}>delete</button>
             <button onClick = {this.editRecipe}>edit</button>
             </li>
           })}  
-        </ul>  
+        </ul>   */}
+         {this.state === "loading" ? <p>Loading...</p> :   
+          <div  className="container-favList">
+            <h1>You have {createdList.length} recipes!</h1>
+              <div className="recipe-box">
+              {createdList.map((recipe) => {
+                return <div className="recipe-box-recipe" key={recipe._id}>
+                          <div className="recipeBox-title">
+                            <h2>{recipe.recipeName}</h2>
+                          </div>
+                          <div className="recipeBox-video">
+                            <iframe
+                              width='200'
+                              height='150'
+                              src={recipe.videoLink}
+                              title='YouTube video player'
+                              frameBorder='0'
+                              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                              allowFullScreen
+                            ></iframe>
+                          </div>
+                          <div className="recipeBox-btns">
+                            <Link to = {`/recipes/${recipe._id}/details`}>
+                            <button>See details</button>
+                            </Link>
+                            <button onClick={this.handleDelete}>🗑️</button>                     
+                          </div>
+                      </div>
+            })}  
+          </div>  
+        </div>
+      }
       </>
     )
   };
