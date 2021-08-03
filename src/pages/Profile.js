4d5763f54e2fa86c582a9a6ac5d9c32e54e20b1c
Borjaarmet <borjaarmet@gmail.com>
2021-08-03 12:51:30 +0200

    fixed bugs

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -95,18 +95,15 @@ import Navbar from '../components/Navbar';
                   value={user.age}
                   onChange={this.handleChange}
                   />
-            
-              <label className='label-profile' htmlFor='cookLevel'>Cook level <br /></label>
-             
-              <select name='cookLevel' id='cookLevel' className="input-signup" value={user.cookLevel}>
-                <option value='none' onChange={this.handleChange}></option>
-                <option value='I have no idea'  onChange={this.handleChange}>I have no idea</option>
-                <option value='I defend myself'  onChange={this.handleChange}>I defend myself</option>
-                <option value='Advanced '  onChange={this.handleChange}>Advanced</option>
-                <option value='I´m a chef'  onChange={this.handleChange}>I´m a chef</option>
-              </select>
-              
-              <button className='signupbtn' type='submit'>Save Changes</button>
+                  <label className='label-profile' htmlFor='cookLevel'>Cook level <br /></label>
+                  <select name='cookLevel' id='cookLevel' className="input-signup" value={user.cookLevel}>
+                    <option value='none' onChange={this.handleChange}></option>
+                    <option value='I have no idea'  onChange={this.handleChange}>I have no idea</option>
+                    <option value='I defend myself'  onChange={this.handleChange}>I defend myself</option>
+                    <option value='Advanced '  onChange={this.handleChange}>Advanced</option>
+                    <option value='I´m a chef'  onChange={this.handleChange}>I´m a chef</option>
+                  </select>
+                <button className='signupbtn' type='submit'>Save Changes</button>
               </div>
              </form>
              </div>
